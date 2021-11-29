fb5841250a29b2370034e8074de74d8804cffcde
kalunge <muthomititus+github@gmail.com>
2021-11-29 14:54:49 +0100

    Generate hello pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'landing#index'
+  root 'pages#hello'
 end
